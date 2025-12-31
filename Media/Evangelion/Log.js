c9fcea68918549cd0d81d955ff7a2ed73721abc9
Raikashi <rashadrampersad7@gmail.com>
2025-12-31 04:11:09 -0400

    modified: requirements.txt

--- Media/Evangelion/Log.js
+++ Media/Evangelion/Log.js
@@ -1,5 +1,5 @@
 {
-  "videos": [
+  "videos"; [
     { "name": "Episode 1", "url": "https://drive.usercontent.google.com/download?id=1u9bfuTzDQLmDNO3oY6WzO7MYAayDif_H&export=download&authuser=0" },
   ]
 }
